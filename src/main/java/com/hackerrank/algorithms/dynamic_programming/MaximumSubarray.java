0b8ddb7be4a891f9763a29c593a3c10d17d3a4d2
Erich Cervantez <git@noofusion.com>
2015-10-22 18:28:35 -0700

    Adding MaxSubarray solution

---
+++ src/main/java/com/hackerrank/algorithms/dynamic_programming/MaximumSubarray.java
@@ -0,0 +1,44 @@
+package main.java.com.hackerrank.algorithms.dynamic_programming;
+
+import java.util.*;
+
+/**
+ * https://www.hackerrank.com/challenges/maxsubarray
+ * <p>
+ * Key points:
+ * • Don't initialize with zero.  Zero would be used in the comparisons.
+ */
+public class MaximumSubarray
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        String answer = "";
+        for (int i = 0; i < T; i++)
+        {
+            int N = scanner.nextInt();
+            int[] A = new int[N];
+            for (int x = 0; x < N; x++)
+            {
+                A[x] = scanner.nextInt();
+            }
+
+            int max_so_far = A[0] < 0 ? A[0] : 0;
+            int max_ending_here = 0;
+            int non_contig_max_so_far = A[0] < 0 ? A[0] : 0;
+            for (int a = 0; a < N; a++)
+            {
+                max_ending_here = max_ending_here + A[a];
+                if (max_so_far < max_ending_here)
+                    max_so_far = max_ending_here;
+                if (max_so_far < A[a])
+                    max_so_far = A[a];
+                if (A[a] > 0)
+                    non_contig_max_so_far += A[a];
+            }
+
+            System.out.println(max_so_far + " " + non_contig_max_so_far);
+        }
+    }
+}
